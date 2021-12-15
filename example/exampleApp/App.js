456a5c1a4c51809a02c293d154773ae0cc971bf2
Elyon <ElyonOlaniran@outlook.com>
2021-12-15 15:47:57 -0800

    working example

--- example/exampleApp/App.js
+++ example/exampleApp/App.js
@@ -1,112 +1,63 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+/* eslint-disable react-native/no-inline-styles */
+import React, { useState } from 'react';
+import { View, Text, Pressable } from 'react-native';
+import { SegmentedArc } from '@shipt/react-native-segmented-arc';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const App = () => {
+  const [showArcRanges, setShowArcRanges] = useState(false);
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  const segments = [
+    {
+      scale: 0.25,
+      filledColor: '#FF746E',
+      emptyColor: '#F2F3F5',
+      data: { label: 'Red' }
+    },
+    {
+      scale: 0.25,
+      filledColor: '#F5E478',
+      emptyColor: '#F2F3F5',
+      data: { label: 'Yellow' }
+    },
+    {
+      scale: 0.25,
+      filledColor: '#78F5CA',
+      emptyColor: '#F2F3F5',
+      data: { label: 'Green' }
+    },
+    {
+      scale: 0.25,
+      filledColor: '#6E73FF',
+      emptyColor: '#F2F3F5',
+      data: { label: 'Blue' }
+    }
+  ];
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+  const ranges = ['10', '20', '30', '40', '50'];
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const _handlePress = () => {
+    setShowArcRanges(!showArcRanges);
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <SegmentedArc
+        segments={segments}
+        fillValue={70}
+        isAnimated={true}
+        animationDelay={1000}
+        showArcRanges={showArcRanges}
+        ranges={ranges}
+      >
+        {metaData => (
+          <Pressable onPress={_handlePress} style={{ alignItems: 'center' }}>
+            <Text style={{ fontSize: 16, paddingTop: 16 }}>{metaData.lastFilledSegment.label}</Text>
+            <Text style={{ lineHeight: 80, fontSize: 24 }}>More info</Text>
+          </Pressable>
+        )}
+      </SegmentedArc>
+    </View>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
