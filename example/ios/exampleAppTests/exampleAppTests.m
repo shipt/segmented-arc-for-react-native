a07b89494fa9fe6cb243178395dfdb0e21b7db4a
Josh Griffin <joshuakgriff@gmail.com>
2024-03-27 12:42:47 -0500

    update to exampleApp and add pod script for hermes

--- example/ios/exampleAppTests/exampleAppTests.m
+++ example/ios/exampleAppTests/exampleAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ExampleAppTests : XCTestCase
+@interface exampleAppTests : XCTestCase
 
 @end
 
-@implementation ExampleAppTests
+@implementation exampleAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
