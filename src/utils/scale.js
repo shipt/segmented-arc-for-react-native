f0b75797695812d64e79c3300d9a5242a81b27e4
Roni Castro <ronidecastro@shipt.com>
2025-01-07 19:33:42 -0300

    Move internal functions to the top and and apply undescore

--- src/utils/scale.js
+++ src/utils/scale.js
@@ -1,25 +1,26 @@
-export const ensureDefaultSegmentScale = segments => {
-  const isInvalidScale = value => !Number.isFinite(value) || value === 0;
+const _isInvalidScale = value => !Number.isFinite(value) || value === 0;
+const _isInvalidArcDegreeScale = value => !Number.isFinite(value);
 
-  const invalidScaleCount = segments.filter(({ scale }) => isInvalidScale(scale)).length;
-  const allocatedScale = segments.reduce((sum, { scale }) => sum + (isInvalidScale(scale) ? 0 : scale), 0);
+export const ensureDefaultSegmentScale = segments => {
+  const invalidScaleCount = segments.filter(({ scale }) => _isInvalidScale(scale)).length;
+  const allocatedScale = segments.reduce((sum, { scale }) => sum + (_isInvalidScale(scale) ? 0 : scale), 0);
   const defaultArcScale = invalidScaleCount > 0 ? (1 - allocatedScale) / invalidScaleCount : 0;
 
-  return segments.map(segment => (isInvalidScale(segment.scale) ? { ...segment, scale: defaultArcScale } : segment));
+  return segments.map(segment => (_isInvalidScale(segment.scale) ? { ...segment, scale: defaultArcScale } : segment));
 };
 
 export const ensureDefaultSegmentArcDegreeScale = segments => {
-  const isInvalidArcDegreeScale = value => !Number.isFinite(value);
-
-  const invalidArcDegreeCount = segments.filter(({ arcDegreeScale }) => isInvalidArcDegreeScale(arcDegreeScale)).length;
+  const invalidArcDegreeCount = segments.filter(({ arcDegreeScale }) =>
+    _isInvalidArcDegreeScale(arcDegreeScale)
+  ).length;
   const allocatedDegreeScale = segments.reduce(
-    (sum, { arcDegreeScale }) => sum + (isInvalidArcDegreeScale(arcDegreeScale) ? 0 : arcDegreeScale),
+    (sum, { arcDegreeScale }) => sum + (_isInvalidArcDegreeScale(arcDegreeScale) ? 0 : arcDegreeScale),
     0
   );
   const defaultArcDegreeScale = invalidArcDegreeCount > 0 ? (1 - allocatedDegreeScale) / invalidArcDegreeCount : 0;
 
   return segments.map(segment =>
-    isInvalidArcDegreeScale(segment.arcDegreeScale) ? { ...segment, arcDegreeScale: defaultArcDegreeScale } : segment
+    _isInvalidArcDegreeScale(segment.arcDegreeScale) ? { ...segment, arcDegreeScale: defaultArcDegreeScale } : segment
   );
 };
 
