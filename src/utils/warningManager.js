6fa53ae30ee0064c04c038ad203202b1bd027b48
Roni Castro <ronidecastro@shipt.com>
2025-01-08 13:51:52 -0300

    Remove unecessary showWarningOnce

--- src/utils/warningManager.js
+++ src/utils/warningManager.js
@@ -1,25 +1,7 @@
-const warnings = new Set();
-
-const showWarningOnce = (data, warnId = '') => {
-  const message = data instanceof Error ? data.message : String(data);
-  const key = message + warnId;
-
-  if (!warnings.has(key)) {
-    console.warn(data);
-    warnings.add(key);
-  }
-};
-
-const clearAllWarnings = () => {
-  warnings.clear();
-};
-
 const showWarning = data => {
   console.warn(data);
 };
 
 export default {
-  showWarning,
-  showWarningOnce,
-  clearAllWarnings
+  showWarning
 };
