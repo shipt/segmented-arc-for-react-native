ab3a4b812e82a559714e8262fe0d9e9ce25b2611
Roni Castro <ronidecastro@shipt.com>
2025-01-29 17:45:20 -0300

    Revert "Revert "Remove unecessary showWarningOnce""
    This reverts commit 392e81bd4bf73f9aceb15aef0e1fab08b0a150ab.

--- src/utils/warningManager.js
+++ src/utils/warningManager.js
@@ -1,24 +1,7 @@
-const warnings = new Set();
-
-const showWarningOnce = data => {
-  const key = data instanceof Error ? data.message : String(data);
-
-  if (!warnings.has(key)) {
-    console.warn(data);
-    warnings.add(key);
-  }
-};
-
-const clearAllWarnings = () => {
-  warnings.clear();
-};
-
 const showWarning = data => {
   console.warn(data);
 };
 
 export default {
-  showWarning,
-  showWarningOnce,
-  clearAllWarnings
+  showWarning
 };
