fd46e25ad3dbd0344145b7d4d9d0e34331534206
Jakhongir Khusanov <25942541+jkhusanov@users.noreply.github.com>
2025-03-04 17:24:31 -0800

    Upgrade RN version to 0.78 (#118)
    * Upgrade RN version to 0.78"
    
    * format with prettier
    
    * remove test

--- example/android/app/src/main/java/com/example/MainApplication.kt
+++ example/android/app/src/main/java/com/example/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.exampleapp
+package com.example
 
 import android.app.Application
 import com.facebook.react.PackageList
@@ -9,6 +9,7 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
@@ -30,11 +31,11 @@ class MainApplication : Application(), ReactApplication {
       }
 
   override val reactHost: ReactHost
-    get() = getDefaultReactHost(this.applicationContext, reactNativeHost)
+    get() = getDefaultReactHost(applicationContext, reactNativeHost)
 
   override fun onCreate() {
     super.onCreate()
-    SoLoader.init(this, false)
+    SoLoader.init(this, OpenSourceMergedSoMapping)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
