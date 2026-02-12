61e0004d7c7f4c7f59471f4a3d9c882d52399ddb
Jahon Khusanov <25942541+jkhusanov@users.noreply.github.com>
2026-02-12 11:05:31 -0800

    Make the example project run only local changes

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,4 +1,9 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
+
+// Find the project root
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, '..');
 
 /**
  * Metro configuration
@@ -6,6 +11,16 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+
+const config = {
+  watchFolders: [workspaceRoot],
+  resolver: {
+    nodeModulesPaths: [
+      path.resolve(projectRoot, 'node_modules'),
+      path.resolve(workspaceRoot, 'node_modules'),
+    ],
+    disableHierarchicalLookup: true,
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
